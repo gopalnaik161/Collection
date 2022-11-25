261cfb65d0c0b185a01f00854ddc5069bed42ca2
Gopalkrishna <111700016+gopalnaik161@users.noreply.github.com>
2022-11-25 16:22:21 +0530

    Add files via upload

---
+++ practiceCollection/src/A.java
@@ -0,0 +1,208 @@
+
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+
+//import java.util.*;
+//
+//public class A {
+//    public static void main(String[] args) {
+//
+////        Stack<String> e =new Stack<>();
+////        e.push("zdr");
+////        e.push("qwe");
+////        e.pop();
+////        e.push("nko");
+////
+////
+////
+////        Iterator<String>q=e.iterator();
+////        while (q.hasNext()){
+////            System.out.println(q.next());
+////
+////        }
+//
+////
+////        PriorityQueue<String> s= new PriorityQueue<>();
+////        s.add("qwe");
+////
+////        s.add("poi");
+////        s.add("qsc");
+////        s.add("pln");
+////        s.add("qweeef");
+////
+////        System.out.println( s.peek());
+////
+////        Iterator<String> x= s.iterator();
+////        while(x.hasNext()){
+////            System.out.println(x.next());
+////        }
+////
+////        HashSet<String> x =new HashSet<>();
+////        x.add("abc");
+////        x.add("abc");
+////        x.add("zdr");
+////        x.add("qwe");
+////
+////        Iterator<String > a = x.iterator();
+////        while (a.hasNext()) {
+////
+////            System.out.println(a.next());
+////        }
+//
+////        ArrayList<String> x= new ArrayList<>();
+////
+//////        x.ensureCapacity(20);
+////        x.add("ase");
+////        x.add("uhg");
+////        x.add("pkm");
+////        x.add("yui");
+//
+////        x.trimToSize();
+////
+////        System.out.println(x);
+////
+////        System.out.println("size of array list :" + x.size());
+//
+////        System.out.println(x.isEmpty());
+//
+// //       System.out.println(x.contains("ase"));
+////        System.out.println(x);
+////
+////        Object[]array =x.toArray();
+////        for(Object o : array){
+////            System.out.println(o);
+////        }
+//
+////x.set(1,"goapl");
+////System.out.println(x);
+//
+////
+////        LinkedList<String>a = new LinkedList<>();
+////
+////        a.add("zse");
+////        a.add("nko");
+////        a.add("qwe");
+////        a.add("yui");
+////        a.add("ert");
+//////
+////       System.out.println(a);
+//
+////        String s="plm";
+////
+////        boolean conatins = a.contains(s);
+////        if(conatins){
+////            System.out.println(a.indexOf(s));
+////        }else{
+////            System.out.println("word is not present ");
+////        }
+////
+////
+////         Iterator<String> u = a.descendingIterator();
+////
+////         while(u.hasNext()){
+////             System.out.println(u.next());
+////         }
+////
+////        ArrayList<String> s =new ArrayList<>();
+////
+////        s.add("gopal");
+////        s.add("rakesh");
+////        s.add("raju");
+////        s.add("sanju");
+////
+////        System.out.println(s);
+//
+//
+////        LinkedList<Integer> queue = new LinkedList<>();
+////
+////        queue.add(10);
+////        queue.add(90);
+////        queue.add(45);
+////        queue.add(87);
+//////
+////        System.out.println(queue);
+////        System.out.println(queue.poll());
+////        System.out.println(queue.poll());
+//
+//        LinkedList<Integer> x= new LinkedList<>();
+//        x.add(10);
+//        x.add(90);
+//        x.add(45);
+//        x.add(87);
+//
+//
+//        System.out .println(x);
+//
+//        x.offerFirst(19);
+//
+//        System.out.println(x);
+//        x.addLast(88);
+//
+//        System.out.println(x);
+
+
+
+
+
+
+
+//   }
+//}
+
+//public class A{
+//    public static void main(String[] args) {
+//        HashMap<Integer,String>x= new HashMap<>();
+//
+//        x.put(1,"cgt");
+//        x.put(2,"poiuy");
+//        x.put(3,"qwer");
+//        x.put(4,"fgyu");
+//
+//        System.out.println(x);
+//
+//        Set<Map.Entry<Integer, String>> entrySet = x.entrySet();
+//        for (Map.Entry<Integer, String> entry : entrySet)
+//        {
+//            System.out.println(entry.getKey()+" : "+entry.getValue());
+//
+//
+//        }
+//
+//        HashMap<Integer,String> S= new HashMap<>();
+//
+//        S.put(5,"nbv");
+//        S.put(7,"qwe");
+//
+//        Set<Map.Entry<Integer, String>> entrySet1 = S.entrySet();
+//
+//
+//
+//    }
+//}
+
+
+//
+//public class A
+//{
+//    public static void main(String[] args)
+//    {
+//
+//        HashMap<String, Integer> map = new HashMap<String, Integer>();
+//        map.put("ONE", 1);
+//        map.put("TWO", 2);
+//        map.put("THREE", 3);
+//        map.put("FOUR", 4);
+//
+//        int value = map.get("TWO");
+//
+//        System.out.println(value);
+//
+//
+//    }
+//}
+
+
